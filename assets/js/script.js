d13f3f92e8191628e88129a8653113cdf6197843
Kevin Chang <kchang96@berkeley.edu>
2018-04-24 01:00:21 -0700

    Do topics page layout and functionality.

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    
+    const TOPICS = ["Intro to Java.",  "Generics.", "Linked Lists.",
+                    "Trees.", "Graphs.", "Sorting."];
+    const SITENAME = "https://changkonabe.github.io/"
+    
+    for (let i = 0; i < TOPICS.length; i++) {
+        addTopic(TOPICS[i]);
+    }
+
+    // jQuery Function Number
+    $(".box").click(function() {
+        let topic = $(this)[0].innerText.trim();
+        let cleaned = topic.replace(/\s/g, "-").replace(/\./,".html");
+        let redirect = SITENAME + "topics/" + cleaned;
+        window.location.href = redirect;
+    });
+    
+    function addTopic(name) {
+        // jQuery Function Number
+        $("#topics-container").append(
+            '<div id="topic" class="box"><div id="topic-content"><h1>' + name + '</h1></div></div>'
+        );
+    }
+
+})
+
